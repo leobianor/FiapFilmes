0f3f12670d9814904d3f96d69cf0a24cd4d54ad2
leobianor <89613674+leobianor@users.noreply.github.com>
2023-08-18 09:14:54 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
-import Image from 'next/image'
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
-  return ( //JSX
-    <nav className="bg-slate-500 p-2 flex gap-3 items-end">
-      <h1 className='text-4xl text-zinc-100 font-bold uppercase'>
-        FIAP Filmes
+  return ( 
+    <>
+    <nav className="bg-neutral-950 p-2 flex gap-3 items-end">
+      <h1 className='text-4xl text-zinc-100 font-bold uppercase mx-8'>
+        Fiap Filmes
       </h1>
 
       <ul>
         <li>
-          <a href="#">Favoritos</a>
-          <a href="#">Lançamentos</a>
+          <a className='mx-8' href="#">Favoritos</a>
+          <a className='mx-8' href="#">Lançamentos</a>
         </li>
       </ul>
     </nav>
+    <Titulo texto = "Em Alta" />
+    <Titulo texto = "Lançamentos" />
+
+    </>
   )
 }
---
+++ src/components/Titulo.js
@@ -0,0 +1,8 @@
+export default function Titulo({texto}) {
+    return (
+        <h2 className='text-2xl border-l-8 m-4 border-pink-600 pl-2 leading-9'>
+            {texto}
+        </h2>
+    )
+
+}
